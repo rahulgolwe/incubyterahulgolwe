60fbaee0aa4cc37e93065949b100dbde90d82acd
rahulgolwe <rahulgolwe98@gmail.com>
2024-12-10 00:24:32 +0530

    Implemented add method in StringCalculator class for Empty String case.

--- AssessmentRahulGolwe/src/main/StringCalculator.java
+++ AssessmentRahulGolwe/src/main/StringCalculator.java
@@ -3,4 +3,13 @@ package main;
 
 public class StringCalculator {
 
+	public int add(String numbers) {
+        // Case: Empty string
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        
+        // If there's one number, return its integer value
+        return Integer.parseInt(numbers);
+    }
 }
